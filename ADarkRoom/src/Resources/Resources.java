dd85524bcddc2b7d7e2ed8ab2d22d116f4dbbb8a
Alvin On <alvin.zhou.on@gmail.com>
2015-05-14 21:23:12 -0700

    Fixed many errors, rearranged code

--- ADarkRoom/src/Resources/Resources.java
+++ ADarkRoom/src/Resources/Resources.java
@@ -1,31 +1,16 @@
 package Resources;
 
 public class Resources{
-	private int currentWater,currentFood;
-	private int totalWater,totalFood=100;
 	
-	public Resources(int currentWater,int currentFood){
-		this.currentFood=currentFood;
-		this.currentWater=currentWater;
+	private int amount;
+	
+	public Resources(int amount){
+		this.setAmount(amount);
 	}
+
+	public int getAmount() {return amount;}
+	public void setAmount(int amount) {this.amount = amount;}
+	public void subractAmount(int amount) {this.amount -= amount;}
+	public void addAmount(int amount) {this.amount += amount;}
 	
-	public void checkLimitResources(){
-		if(currentFood>totalFood)
-		{
-			currentFood=totalFood;
-		}
-		if(currentWater>totalWater)
-		{
-			currentWater=totalWater;
-		}	
-	}
-	
-	public int getCurrentWater() {return currentWater;}
-	public void setCurrentWater(int currentWater) {this.currentWater = currentWater;}
-	public int getTotalWater() {return totalWater;}
-	public void setTotalWater(int totalWater) {this.totalWater = totalWater;}
-	public int getCurrentFood() {return currentFood;}
-	public void setCurrentFood(int currentFood) {this.currentFood = currentFood;}
-	public int getTotalFood() {return totalFood;}
-	public void setTotalFood(int totalFood) {this.totalFood = totalFood;}
 }
